2907d893e67ded1bcf263aa6c474cca2108fe225
kibilis7 <kibilis7@gmail.com>
2020-04-23 19:45:43 +0900

    test upload

--- app/src/main/java/org/techtown/samplelinearlayout/LayoutCodeActivity.java
+++ app/src/main/java/org/techtown/samplelinearlayout/LayoutCodeActivity.java
@@ -8,6 +8,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class LayoutCodeActivity extends AppCompatActivity {
 
+    //test
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
